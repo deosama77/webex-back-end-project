7341f9f041523d7c2651d9201e90e8082cf86069
deosama77 <deosama77@gmail.com>
2020-07-26 15:36:27 +0300

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,25 @@
-DROP DATABASE IF EXISTS webex_product;
-CREATE DATABASE webex_product;
+-- Table: public.projects
 
-DROP TABLE IF EXISTS products;
-CREATE TABLE products(
-   id SERIAL PRIMARY KEY,
-   name TEXT,
-   status_progress TEXT,
-   status_provider TEXT,
-   complicity  INTEGER NOT NULL,
-   owner_id INTEGER NOT NULL,
-   resources INTEGER,
-   price INTEGER,
-   provider TEXT
-   start_date date,
-   end_date date
-);
+-- DROP TABLE public.projects;
 
+CREATE TABLE public.projects
+(
+    id integer NOT NULL DEFAULT nextval('products_id_seq'::regclass),
+    name text COLLATE pg_catalog."default",
+    status_progress text COLLATE pg_catalog."default",
+    status_provider text COLLATE pg_catalog."default",
+    complicity integer NOT NULL,
+    owner_id integer,
+    resources integer,
+    price integer,
+    provider text COLLATE pg_catalog."default",
+    start_date date,
+    end_date date,
+    offers numeric,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+)
 
+TABLESPACE pg_default;
 
+ALTER TABLE public.projects
+    OWNER to postgres;
